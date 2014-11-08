b24ca6230d8be16c6360afa3300e91be005e4ab6
Charles Renwick <crenwick@crenwick-mac.local>
2014-11-07 18:26:45 -0800

    added more

--- c6.1.js
+++ c6.1.js
@@ -13,3 +13,11 @@ cities.remove('carl');
 cities.display();
 console.log('revserse: ');
 console.log(cities.dispReverse());
+//implent a advance(n) function that moves the node forward
+//implement a back(n)
+//implement a show(node) function
+
+//write a program to keep track of a set of test grades
+
+//40 roman soldiers in a circle
+//kill every third
